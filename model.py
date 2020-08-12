345a7daf0fa6b948d23b7865091b4107dd02638b
Michelle Chen <mich.chen.94@gmail.com>
2020-08-11 19:33:30 -0700

    connect to db

--- model.py
+++ model.py
@@ -111,4 +111,16 @@ class Instructions(db.Model):
 
 
 
+def connect_to_db(flask_app, db_uri='postgresql:///recipes', echo=True):
+    flask_app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+    flask_app.config['SQLALCHEMY_ECHO'] = echo
+    flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.app = flask_app
+    db.init_app(flask_app)
+
+    print('Connected to the db!')
+
+
+
 
