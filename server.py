2388192fede3852b794acf1051745ef943459644
Michelle Chen <mich.chen.94@gmail.com>
2020-08-11 23:25:52 -0700

    added logout route, set up saved recipes route, tested login with event handling

--- server.py
+++ server.py
@@ -2,7 +2,7 @@
 
 
 # importing all these for now
-from flask import (Flask, render_template, request, flash, session, redirect)
+from flask import (Flask, render_template, request, flash, session, redirect, jsonify)
 import os
 import requests
 import json
@@ -10,7 +10,6 @@ import json
 # import jinja 2 to make it throw errors for undefined variables
 from jinja2 import StrictUndefined
 
-from pprint import pprint
 
 # instance of Flask class, store as app
 app = Flask(__name__)
@@ -36,18 +35,37 @@ def process_login():
 
     email = request.form.get('email')
     password = request.form.get('password')
+    # print(request.is_json)
+    # login_data = request.get_json()
+    # print(login_data.email, login_data.password)
+    # print(email, password)
 
-    session['user_id'] = email
+
+    # session['user_id'] = login_data.email
     flash('logged in!')
 
     # flash('hit login route!')
 
     # function to check if email exists in db
+    # existing_user = User.query.filter_by(email=email).first()
 
-    # conditional if email exists, flash message
-    # else create a new user in db and flash message
-    return redirect("/")
+    # if existing_user and password == existing_user.password:
+    #     flash('Successfully logged in!')
+    # elif existing_user:
+    #     flash('You cannot create an account with that email. Try again.')
+    # else:
+    #     # create a new user in db
+    #     flash('Account created successfully!')
 
+
+    return jsonify({"success": True})
+
+@app.route('/logout')
+def process_logout():
+
+    del session['user_id']
+    flash('logged out!')
+    return redirect('/')
 
 
 @app.route('/search_results', methods=["POST"])
@@ -55,10 +73,10 @@ def search_results():
 
     # User's input is a string of comma-separated list of ingredients 
     input_ingredients_str = request.form.get("user_ingredients")
-    
-    # print(input_ingredients_str)
 
+    # spoonacular's api url
     url = "https://api.spoonacular.com/recipes/complexSearch"
+    # api parameters
     payload = {"apiKey": API_KEY, 
                "includeIngredients": input_ingredients_str,
                "addRecipeInformation": True,
@@ -69,6 +87,7 @@ def search_results():
                # "offset": 5,
                "number": 3,
                } 
+    # make http request to spoonacular's complexSearch API
     res = requests.get(url, params=payload)
     # convert json into python dictionary -> API is a List of dictionaries
     data = res.json()
@@ -76,24 +95,21 @@ def search_results():
 
     recipes = data['results']
 
-    # each list is a list of dictionaries of each step with keys:
-    # 'number', 'step', 'ingredients', 'equipment'
-    # instruction_steps = []
-
-
     # for recipe in recipes:
     #     instructions = recipe['analyzedInstructions'][0]['steps']
     #     # instructions is list of dictionaries which are each step
-    #     # instruction_steps.append(instructions)
     #     print(f'number of steps: {len(instructions)}')
     #     for step in instructions:
     #         print(step['step'])
     #     print('\n')
 
-
     return render_template("search_results.html", recipes=recipes)
 
 
+@app.route('/saved_recipes')
+def add_recipe_to_saved():
+
+    pass
 
 
 
