43622bb85be1bda1dac19af338deebdfeadd368f
Michelle Chen <mich.chen.94@gmail.com>
2020-08-12 22:23:58 -0700

    complete creating db and seeding with ingredient data from spoonacular csv

--- seed_db.py
+++ seed_db.py
@@ -1,7 +1,9 @@
 """Script to seed database"""
 
-# Import os 
+# Import os and libraries
 import os
+import json
+import csv
 
 # Import dabase model, crud, and server
 import crud
@@ -17,3 +19,15 @@ model.connect_to_db(server.app)
 
 # Create tables from classes inherited from db.model
 model.db.create_all()
+
+# read and open ingredients csv
+file = open('top-1k-ingredients.csv', newline='')
+data = csv.reader(file, delimiter=';')
+# each row is a list of 2, name and id
+ingredients_data = {row[1]:row[0] for row in data}
+# make id as key and name as value
+
+# loop over ingredients dictionary
+for ingredient in ingredients_data:
+    # create an ingredient and save to db
+    crud.create_ingredient(ingredient_id=ingredient, name=ingredients_data[ingredient])
