9fd747322965be53c4b901176639f5b7412664f2
Michelle Chen <mich.chen.94@gmail.com>
2020-08-14 23:08:53 -0700

    imported helper_function.py, added parsing in api view function for easier access

--- server.py
+++ server.py
@@ -14,6 +14,7 @@ from jinja2 import StrictUndefined
 
 from model import connect_to_db
 import crud # operations for db
+import helper_functions
 
 
 # instance of Flask class, store as app
@@ -34,13 +35,13 @@ def homepage():
 
 
 
-@app.route('/login', methods=["POST"])
+@app.route('/api/login', methods=["POST"])
 def process_login():
 
     print('in login route')
     # unencode from JSON
     data = request.get_json()
-    # print(data)
+    # # print(data)
     email = data['email']
     password = data['password']
 
@@ -53,7 +54,7 @@ def process_login():
     # check if email exists in db, if so also check correct password
     if existing_user and password == existing_user.password:
         # create session for user
-        session['user_id'] = email
+        session['email'] = email
         # set new message
         message = 'Valid user. Successfully logged in.'
 
@@ -68,7 +69,7 @@ def process_login():
     else:
         new_user = crud.create_user(email=email, password=password)
         # create session for user
-        session['user_id'] = email
+        session['email'] = email
         # set new message
         message = 'Successfully created new account!'
 
@@ -78,20 +79,25 @@ def process_login():
 
 @app.route('/logout')
 def process_logout():
+    """Remove user's session after logout."""
 
-    del session['user_id']
+    del session['email']
+    del session['recipes']
     flash('logged out!')
     return redirect('/')
 
 
 
-@app.route('/search_results', methods=["POST"])
+@app.route('/api/search_results', methods=["POST"])
 def search_results():
+    """Make API request for search results."""
 
     print("route is hit through js")
-
+    # unencode from JSON
+    data = request.get_json()
+    print(data)
     # User's input is a string of comma-separated list of ingredients 
-    input_ingredients_str = request.form.get("user_ingredients")
+    input_ingredients_str = data['ingredients']
     print(input_ingredients_str)
     # print(request.form)
 
@@ -114,23 +120,97 @@ def search_results():
     data = res.json()
     # print(res.json())
 
-    recipes = data['results']
+    # list of recipes (which are dictionaries about recipe details)
+    recipes_complex_data = data['results']
+    # print(type(recipes_complex_data))
+    # pprint(recipes_complex_data)
 
-    return render_template("search_results.html", recipes=recipes)
+    # parse only details we need from api endpoint
+    recipes_info = helper_functions.parse_recipe_details(recipes_complex_data)
+
+    # list of dictionaries indentifiable with recipe's id and their time info
+    recipe_times = helper_functions.parse_recipe_times(recipes_complex_data)
+    # list of dictionaries indentifiable with recipe's id and their instructions info
+    recipe_instructions = helper_functions.parse_recipe_instructions(recipes_complex_data)
+
+    recipe_equipments = helper_functions.parse_recipe_equipment(recipes_complex_data)
+
+    # store recipe results in current user's session
+    session['recipes_info'] = recipes
+    session['recipe_times'] = recipe_times
+    session['recipe_instructions'] = recipe_instructions
+
+    # return render_template("search_results.html", recipes=recipes)
+    return jsonify({'recipes_info': recipes_info, 'recipe_times': recipe_times, 'recipe_instructions': recipe_instructions, 'recipe_equipments': recipe_equipments})
+
+
+# @app.route('/api/show_results')
+# def show_search_results():
+#     """Show recipe search results."""
+
+#     reipces = 
 
 
 
-@app.route('/save_a_recipe')
+
+
+
+
+
+
+
+
+
+@app.route('/save_a_recipe',methods=["POST"])
 def add_recipe_to_saved():
     """Add selected recipe to database as saved recipe."""
-    pass
+
+    # recipe_id = request.form.get('recipe_id')
+    # # retrieve session's: recipe search results, user's email
+    # # recipe_results = session['recipes']
+    # if session['email']:
+    #     user = crud.get_user_by_email(session['email'])
+    # else:
+
+
+
+    # need to parse through recipe results for appropriate info
+
+    # adds user's selected recipe to saved recipes table, is_favorite is false until favorited after saving recipe
+    # crud.save_a_recipe(user=user.user_id, recipe=recipe, is_favorite=False)
+
+    # # add this recipe to the recipe, instructions, and recipe's ingredients table
+    # crud.create_recipe(title=title, image=image, servings=servings)
+    # crud.add_instructions(recipe=recipe_id, step_num=step_num, instruction=instruction, cooking_mins=cooking_mins, prep_mins=prep_mins, ready_mins=ready_mins, equipement=equipment)
+    # crud.add_recipe_ingredient(recipe=recipe_id, ingredient=)
+    
+    return jsonify({'success': True})
+
 
 
 
 @app.route('/saved_recipes')
 def show_users_saved_recipes():
     """Show all of user's saved recipes."""
+
+    # all_saved_recipes = crud.show_saved_recipes()
+
+    # recipe_ids = []
+
+    # for recipe in all_saved_recipes:
+    #     recipe_ids[recipe.recipe_id] = 
+
     pass
+
+
+@app.route('/favorited')
+def favorite_a_recipe():
+    """Actively favorite a recipe.
+
+    set is_favorite in db to True."""
+
+    pass
+
 
 
 
